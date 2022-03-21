659a52dbab5326c52d2378b2de5cbb1eb8f7511c
Dan Grossberg <dwgrossberg@gmail.com>
2022-03-21 22:28:36 +0700

    Setup form input into new Book object

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
-const read = document.getElementById('read');
-const recommend = document.getElementById('recommend');
-const library = document.getElementsByClassName('library');
+const form = document.getElementById('form');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const bookRead = document.getElementById('read');
+const bookRecommend = document.getElementById('recommend');
+const bookDeposit = document.getElementsByClassName('deposit');
+const bookLibrary = document.getElementsByClassName('library');
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read, recommend) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
+    this.recommend = recommend;
 }
 
-function addBookToLibrary() {
-    // intake user input
-    // create new Book object
+function addBookToLibrary(e) {
+    e.preventDefault();
+    let newTitle = bookTitle.value;
+    let newAuthor = bookAuthor.value;
+    let newPages = bookPages.value;
+    let newRead = bookRead.value;
+    let newRecommend = bookRecommend.value;
+    let newBook = new Book(newTitle, newAuthor, newPages, newRead, newRecommend)
+    console.log(newBook);
     // store new Book object in myLibrary
 }
+
+form.addEventListener('submit', addBookToLibrary);
