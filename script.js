3d745fa9cf826ea1a4a378989d15bc33b6620c84
Dan Grossberg <dwgrossberg@gmail.com>
2022-03-22 16:07:03 +0700

    Setup baseline stock of books for library

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const library = document.getElementById('library');
 const libraryBook = document.getElementsByClassName('book');
 
 let myLibrary = [];
+let myBooks = [];
 
 function Book(title, author, pages, read, recommend) {
     this.title = title;
@@ -12,25 +13,36 @@ function Book(title, author, pages, read, recommend) {
     this.recommend = recommend;
 }
 
+const book1 = new Book('Mirrors: Stories of Almost Everyone', 'Eduardo Galeano', 401, 'no');
+const book2 = new Book('Henry and June', 'Anais Nin', 281, 'yes', 'yes');
+const book3 = new Book('Journey to the End of the Night', 'Louis-Ferdinand Céline', 464, 'yes', 'maybe-so');
+myBooks.push(book1, book2, book3);
+myBooks.forEach(book => {
+    myLibrary.push(book);
+    displayLibrary();
+})
+
 function addBookToLibrary(e) {
     e.preventDefault();
     // store user values
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
-    let read = document.querySelector('input[name="read"]:checked').value;
+    let read = document.querySelector('input[id="read"]:checked').value;
     let recommend = document.querySelector('input[name="recommend"]:checked').value;
     let book = new Book(title, author, pages, read, recommend)
     // store the new Book object to myLibrary
     myLibrary.push(book);
     console.log(book, myLibrary);
     // add the new Book object to the library DOM
-    displayLibrary();
     form.reset();
+    displayLibrary();
 }
 
 function displayLibrary() {
     myLibrary.forEach((val, key, arr) => {
+        
+        
         if (Object.is(arr.length -1 , key)) {
             let bookArray = Object.values(val);
             let newBook = document.createElement('div');
